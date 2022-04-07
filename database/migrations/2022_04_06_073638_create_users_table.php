3efb606c794a502200516e3203f7a66b1b95b731
Eugene <eugene.rim911@gmail.com>
2022-04-07 14:58:42 +0500

    Add phone field to user migration

--- database/migrations/2022_04_06_073638_create_users_table.php
+++ database/migrations/2022_04_06_073638_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('phone')->nullable();
             $table->unsignedBigInteger('role_id');
             $table->rememberToken();
             $table->timestamps();
