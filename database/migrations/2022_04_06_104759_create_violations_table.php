2a2cb4f1530dc6bb6cda614d2627a7c8a09b1382
Eugene <eugene.rim911@gmail.com>
2022-04-07 14:28:20 +0500

    Initial commit

---
+++ database/migrations/2022_04_06_104759_create_violations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('violations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('violation_category_id');
+            $table->timestamps();
+            $table->foreign('violation_category_id')->references('id')->on('violation_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('violations');
+    }
+};
---
+++ database/migrations/2022_04_06_164761_create_apllications_cold_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apllications_cold', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('operator_id')->nullable();
+            $table->unsignedBigInteger('magazine_id');
+            $table->unsignedBigInteger('department_id');
+            $table->unsignedBigInteger('violation_id');
+            $table->string('comment')->nullable();
+            $table->unsignedBigInteger('status_id');
+            $table->text('logs')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('operator_id')->references('id')->on('users');
+            $table->foreign('magazine_id')->references('id')->on('magazines')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->foreign('violation_id')->references('id')->on('violations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apllications_cold');
+    }
+};
